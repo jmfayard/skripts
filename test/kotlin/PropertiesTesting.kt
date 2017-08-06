81068d56b621b6d543b780cfd87df9eacdf56c51
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2017-08-06 13:15:55 +0200

    Properties Testing

--- test/kotlin/PropertiesTesting.kt
+++ test/kotlin/PropertiesTesting.kt
@@ -1,6 +1,8 @@
 import com.squareup.moshi.Moshi
 import io.kotlintest.properties.Gen
+import io.kotlintest.specs.FreeSpec
 import io.kotlintest.specs.StringSpec
+import java.math.BigInteger
 import java.util.*
 
 /*
@@ -10,26 +12,72 @@ import java.util.*
 
 
 
-fun String.isPalindrome() : Boolean {
-    for (i in 0 until length) {
-        if (get(i) != get(length-i-1)) return false
+class PropertiesTesting : FreeSpec() { init {
+
+
+    "Different paths, same destination" - {
+        fun List<Int>.mysort() = this.sorted()
+//        fun List<Int>.mysort() = this.filterNot { it == 10 }.sorted()
+//        fun List<Int>.mysort() = emptyList<Int>()
+//        fun List<Int>.mysort() = this.reversed()
+
+        val gen = Gen.list(Gen.choose(8, 267))
+
+        "Size don't change" {
+            forAll(gen) { list ->
+                list.mysort().size == list.size
+            }
+        }
+
+        "Pairs are ordered" {
+            forAll(gen) { list ->
+                list.mysort().fold(Int.MIN_VALUE) { previous, value ->
+                    if (value < previous) return@forAll false
+                    value
+                }
+                true
+            }
+        }
+
+        "Shuffling order makes no difference" {
+            forAll(gen) { list ->
+                list.reversed().mysort() == list.mysort()
+            }
+        }
+
+        "Adding one to all elements" {
+            fun List<Int>.increment() = this.map { it+1 }
+            forAll(gen) { list ->
+                list.mysort().increment() == list.increment().mysort()
+            }
+        }
+
+        "Using the sortedness of the list" {
+            forAll(gen) { list ->
+                (list + Int.MIN_VALUE).mysort() == listOf(Int.MIN_VALUE) + list.mysort()
+            }
+        }
+
+        "Negate then sort" {
+            fun List<Int>.negate() = this.map { -it }
+            forAll(gen) { list ->
+                list.mysort().negate() == list.negate().mysort().reversed()
+            }
+        }
+
+        "There and back again" {
+            forAll(gen) { list ->
+                list.reversed().reversed() == list
+            }
+        }
+
+        "Idempotence" {
+            forAll(gen) { list ->
+                list.mysort() == list.mysort().mysort()
+            }
+        }
     }
-    return true
-}
 
-object GenChar : Gen<Char> {
-    val random = Random()
-
-    private fun Random.nextPrintableChar(): Char {
-        val low = 33
-        val high = 127
-        return (nextInt(high - low) + low).toChar()
-    }
-    override fun generate(): Char = random.nextPrintableChar()
-
-}
-
-class PropertiesTesting : StringSpec() { init {
 
 
     "Property: it never crashes" {
@@ -49,20 +97,76 @@ class PropertiesTesting : StringSpec() { init {
         forAll { t : String ->
             val s = t.take(7) // very few palindroms for large strings
             if (s.isPalindrome()) {
-                println("Found $s")
+                found++
                 s.reversed().isPalindrome()
             } else {
                 !s.reversed().isPalindrome()
             }
         }
+        println("Foupnd $found palindromes")
     }
 
     "Property: three characters" {
-        forAll(GenChar, GenChar) { a, b ->
+        forAll(Gen.char(), Gen.char()) { a, b ->
             "$a$b$a".isPalindrome() && "$a$b$b$a".isPalindrome()
         }
     }
 
+
+    "Hard to prove, easy to verify" {
+        val gen = Gen.list(Gen.choose(1, 100))
+        fun String.parseCommas(): List<String> = this.split(",")
+
+        forAll(gen) { list ->
+            val string = list.joinToString(separator = ",")
+            string.parseCommas() == list.map { it.toString() }
+        }
+    }
+
+    "Dollars" - {
+        val gen = Gen.choose(0, 10000)
+        "inverses" {
+            forAll(gen) { times ->
+                (3*times).dollar == 3.dollar * times
+            }
+        }
+
+        "data class" {
+            forAll(gen) { amount ->
+                amount.dollar == amount.dollar
+            }
+        }
+    }
+
+
 }}
 
 
+
+fun String.isPalindrome() : Boolean {
+    for (i in 0 until length) {
+        if (get(i) != get(length-i-1)) return false
+    }
+    return true
+}
+
+fun Gen.Companion.char() = object : Gen<Char> {
+    val random = Random()
+
+    private fun Random.nextPrintableChar(): Char {
+        val low = 33
+        val high = 127
+        return (nextInt(high - low) + low).toChar()
+    }
+    override fun generate(): Char = random.nextPrintableChar()
+}
+
+val Int.bigint : BigInteger
+    get() = BigInteger.valueOf(this.toLong())
+
+data class Dollar(val amount: Int) {
+    operator fun plus(amount: Int) = Dollar(this.amount +amount)
+    operator fun times(times: Int) = Dollar(this.amount * times)
+}
+val Int.dollar
+    get() = Dollar(this)
